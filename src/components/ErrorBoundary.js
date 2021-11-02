99ace9c1547f8f52b8b2b361ee1c64bdf120705f
Mark Atherton <mark@insightinternet.co.uk>
2021-11-02 11:51:58 +0000

    added componentDidCatch and render methods to ErrorBoundary

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,7 +1,13 @@
 import { Component } from "react";
 
 class ErrorBoundary extends Component {
+  //componentDidCatch triggered whenever a child coponent encounters an error
+  //componentDidCatch method is the ErrorBoundary
   componentDidCatch() {}
+  //add render of this props children so that I can wrap <ErrorBoundary> around any component that should be protected
+  render() {
+    return this.props.children;
+  }
 }
 
 export default ErrorBoundary;
