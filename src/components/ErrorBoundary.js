48eae377e45ef495c268559cc8386170ec992ffa
Mark Atherton <mark@insightinternet.co.uk>
2021-11-02 11:59:00 +0000

    added constructor to ErrorBoundary with initial hasError: false state

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,9 +1,14 @@
 import { Component } from "react";
 
 class ErrorBoundary extends Component {
+  constructor() {
+    super();
+    this.state = { hasError: false };
+  }
+
   //componentDidCatch triggered whenever a child coponent encounters an error
   //componentDidCatch method is the ErrorBoundary
-  componentDidCatch() {}
+  componentDidCatch(error) {}
   //add render of this props children so that I can wrap <ErrorBoundary> around any component that should be protected
   render() {
     return this.props.children;
