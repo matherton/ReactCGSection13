2bf8ad9a17db640d31fc9a0388cb407985dc89de
Mark Atherton <mark@insightinternet.co.uk>
2021-11-02 12:01:32 +0000

    when componentDidCatch encounters an error setState hasError:true

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -8,7 +8,9 @@ class ErrorBoundary extends Component {
 
   //componentDidCatch triggered whenever a child coponent encounters an error
   //componentDidCatch method is the ErrorBoundary
-  componentDidCatch(error) {}
+  componentDidCatch(error) {
+    this.setState({ hasError: true });
+  }
   //add render of this props children so that I can wrap <ErrorBoundary> around any component that should be protected
   render() {
     return this.props.children;
